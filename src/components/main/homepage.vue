b0df744b88bd74f3ca5e3cc905f2b06c802ea23c
justiandevs <justiandev@gmail.com>
2021-02-02 22:21:32 +0100

    Vue setup include bulma setup

---
+++ src/components/main/homepage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="homepage">
+    <navbar/>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import navbar from './navbar.vue'
+
+export default {
+  name: 'homepage',
+  props: {
+    msg: String
+  },
+  components: {
+    navbar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* empty */
+</style>
---
+++ src/components/main/navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-navbar spaced shadow="true">
+      <template #brand>
+          <b-navbar-item tag="router-link" :to="{ path: '/' }">
+              <img
+                  src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                  alt="Lightweight UI components for Vue.js based on Bulma"
+              >
+           </b-navbar-item>
+      </template>
+      <template #start>
+        <b-navbar-item href="#">
+          Over ons
+        </b-navbar-item>
+        <b-navbar-item href="#">
+          Contact
+        </b-navbar-item>
+        <b-navbar-item href="#">
+          Doneren
+        </b-navbar-item>
+      </template>
+      <template #end>
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Registreren</strong>
+            </a>
+            <a class="button is-light">
+              Inloggen
+            </a>
+          </div>
+        </b-navbar-item>
+      </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+}
+</script>
