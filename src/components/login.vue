7c3d59f1a9f6a9182f874c93a0ee46c74bc7be51
justiandevs <justiandev@gmail.com>
2021-02-05 14:50:09 +0100

    Did a lot of work on login view (including adding vee validation). Now only API integration is still needed and some Vuex store things.

--- src/components/login.vue
+++ src/components/login.vue
@@ -3,18 +3,24 @@
         <authnavbar/>
         <div class="container" style="margin-top: 10vh;">
             <div class="columns">
-                <div class="column is-one-quarter is-offset-4">
-                    <h1 class="title">Inloggen</h1>
+                <div class="column is-one-third is-offset-one-third">
+                    <h1 class="title has-text-centered">Inloggen</h1>
                     <div class="card">
                         <!--form-->
                         <div class="card-content">
                             <div class="content">
-                                <form id="form" @submit="checkForm">
-                                    <b-field label="Uw mail">
-                                        <b-input v-model="mail" name="mail"></b-input>
+                                <div v-if="back_errors.length" style="margin-bottom: 1vh;">
+                                    <p class="has-text-danger">Hey, er zijn wat fouten opgetreden bij het verwerken van uw contactformulier!
+                                        <span v-for="back_error in back_errors" :key="back_error">{{ back_error }} </span>
+                                    </p>
+                                </div>
+                                <form id="form" @submit.prevent="checkForm">
+                                    <b-field label="Emailadres" :type="{'is-danger': true, 'is-danger': errors.has('mail')}" :message="errors.first('mail')">
+                                        <b-input v-validate="'required|email'" name="mail" type="mail" v-model="mail" placeholder="Vul hier uw emailadres in..." icon="envelope" icon-pack="fas"></b-input>
                                     </b-field>
-                                    <b-field label="Uw wachtwoord">
-                                        <b-input v-model="password" type="password" name="password" password-reveal></b-input>
+            
+                                    <b-field label="Uw wachtwoord" :type="{'is-danger': true, 'is-danger': errors.has('password')}" :message="errors.first('password')"> 
+                                        <b-input v-validate="'required|min:8|max:50'" v-model="password" type="password" name="password" placeholder="Vul hier uw wachtwoord in..." icon="key" icon-pack="fas"></b-input>
                                     </b-field>
                                     <p class="control">
                                         <b-button class="is-pulled-right" style="margin-bottom: 3vh;" label="Inloggen" type="is-primary" native-type="submit" value="submit"/>
@@ -36,6 +42,28 @@ export default {
     name: "login",
     components: {
         authnavbar
+    },
+    data() {
+        return {
+            password: '',
+            mail: '',
+            back_errors: []
+        }
+    },
+    methods: {
+        checkForm: function() {
+            // Set backend errors to null
+            this.back_errors = [];
+
+            this.$validator.validateAll().then((result) => {
+                if (result) {
+                    this.$validator.reset();
+                    return true;
+                } else {
+                    this.back_errors.push('Je hebt niet alle velden correct ingevuld, probeer het opnieuw.')
+                }
+            });
+        },
     }
 }
 </script>
