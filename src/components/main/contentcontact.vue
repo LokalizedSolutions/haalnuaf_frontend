57a892faeea89563e0aa33ab415dde5c126c4547
justiandevs <justiandev@gmail.com>
2021-02-05 14:13:17 +0100

    Added vee-validate, changed custom validation from contentcontact.vue to vee-validate. Did a lot of fine tuning.

--- src/components/main/contentcontact.vue
+++ src/components/main/contentcontact.vue
@@ -1,24 +1,27 @@
 <template>
     <div>
         <p>Indien u een vraag heeft over ons systeem kan U ons altijd bereiken, ook ontvangen wij graag feedback. Wij doen ons best om zo spoedig mogelijk mailtjes te beantwoorden.</p>
-        <div v-if="errors.length" style="margin-bottom: 1vh;">
+        <div v-if="back_errors.length" style="margin-bottom: 1vh;">
             <p class="has-text-danger">Hey, er zijn wat fouten opgetreden bij het verwerken van uw contactformulier!
-                <span v-for="error in errors" :key="error">{{ error }} </span>
+                <span v-for="back_error in back_errors" :key="back_error">{{ back_error }} </span>
             </p>
         </div>
-        <div v-if="success.length">
-            <p class="has-text-success">Bedankt voor uw bericht, wij proberen zo spoedig mogelijk te antwoorden.</p>
+        <div v-if="success.length" style="margin-bottom: 1vh;">
+            <p class="has-text-success">Dank voor uw bericht, we gaan er zo spoedig mogelijk mee aan de slag.</p>
         </div>
-        <form id="form" @submit="checkForm">
-            <b-field label="Naam + achternaam">
-                <b-input name="name" type="text" v-model="name" placeholder="Vul hier uw naam + achternaam in..." icon="signature" icon-pack="fas"></b-input>
+        <form id="form" @submit.prevent="checkForm">
+            <b-field label="Naam + achternaam" :type="{'is-danger': true, 'is-danger': errors.has('name')}" :message="errors.first('name')">
+                <b-input class="is-input" v-validate="'required|min:3'" name="name" type="text" v-model="name" placeholder="Vul hier uw naam + achternaam in..." icon="signature" icon-pack="fas"></b-input>
             </b-field>
-            <b-field label="Emailadres">
-                <b-input name="mail" type="mail" v-model="mail" placeholder="Vul hier uw emailadres in..." icon="envelope" icon-pack="fas"></b-input>
+            
+            <b-field label="Emailadres" :type="{'is-danger': true, 'is-danger': errors.has('mail')}" :message="errors.first('mail')">
+                <b-input v-validate="'required|email'" name="mail" type="mail" v-model="mail" placeholder="Vul hier uw emailadres in..." icon="envelope" icon-pack="fas"></b-input>
             </b-field>
-            <b-field label="Uw bericht">
-                <b-input name="message" v-model="message" type="textarea" placeholder="Vul hier uw naam + achternaam in..."></b-input>
+            
+            <b-field label="Uw bericht" :type="{'is-danger': true, 'is-danger': errors.has('message')}" :message="errors.first('message')">
+                <b-input v-validate="'required|min:50|max:1000'" name="message" v-model="message" type="textarea" placeholder="Vul hier uw naam + achternaam in..."></b-input>
             </b-field>
+
             <p class="control">
                 <b-button class="is-pulled-right" style="margin-bottom: 3vh;" label="Verzend" type="is-primary" native-type="submit" value="submit"/>
             </p>
@@ -31,55 +34,30 @@ export default {
     name: "contentcontact",
     data() {
         return{
-            errors: [],
-            name: null,
-            mail: null,
-            message: null,
-            success: [],
-            date: null,
+            name: '',
+            mail: '',
+            message: '',
+            back_errors: [],
+            success: []
         }
     },
     methods: {
         // Check contact form
-        checkForm: function(e) {
-            // Set error array to null
-            this.errors = [];
-            this.success = [];
+        checkForm: function() {
+            // Set backend errors to null
+            this.back_errors = [];
 
-            // Check if name, message and email aren't null, and are following our other requirements.
-            if(!this.name) {
-                this.errors.push('U heeft uw naam + achternaam niet ingevuld.');
-            } else if (this.name.length < 3) {
-                this.errors.push('Denk eraan om een naam van 3 of meer karakters te kiezen.')
-            } 
-
-            if(!this.message) {
-                this.errors.push('U heeft geen bericht ingevuld, denk eraan om een bericht in te vullen van minimaal 50 tekens.')
-            } else if (this.message.length < 50 || this.message.length > 1000) {
-                this.errors.push('U kunt alleen berichten sturen die boven de 50 karakters en onder de 1000 karakters lang zijn.')
-            } 
-
-            if(!this.mail) {
-                this.errors.push('U heeft geen mail ingevuld, denk eraan om een email in te vullen.')
-            } else if (!this.validEmail(this.mail)) {
-                this.errors.push('U heeft geen geldige email ingevuld, denk eraan om een geldige email in te vullen.')
-            }
-
-            if(!this.errors.length) {
-                this.sendMail(this.name, this.mail, this.message)
-                // Set everything in form back to null.
-                this.name = null;
-                this.mail = null;
-                this.message = null;
-            }
-
-            // Prevent default
-            e.preventDefault();
-        },
-        // Valid mail check
-        validEmail: function(email) {
-            var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
+            this.$validator.validateAll().then((result) => {
+                if (result) {
+                    this.sendMail(this.name, this.mail, this.message);
+                    this.$validator.reset();
+                    this.name = '';
+                    this.mail = '';
+                    this.message = '';
+                } else {
+                    this.back_errors.push('Je hebt niet alle velden correct ingevuld, probeer het opnieuw.')
+                }
+            });
         },
         // sendMail function
         sendMail: function(name, mail, message) {
@@ -88,7 +66,8 @@ export default {
             // Send axios request (axios is under this.$http)
             this.$http.post("https://dev-api.haalnuaf.nl/mail", { key: this.key, time: this.date, email: mail, name: name, message: message })
                 .then(response => this.success.push('Gelukt' + response))
-                .catch(error => this.errors.push('Er is iets fout gegaan met onze backend, probeer het later opnieuw. Statuscode: ' + error.response.data.status));
+                .catch(error => this.back_errors.push('Er is iets fout gegaan met onze backend, probeer het later opnieuw. Statuscode: ' + error.response.data.status));
+            return true;
         }
     }
 }
