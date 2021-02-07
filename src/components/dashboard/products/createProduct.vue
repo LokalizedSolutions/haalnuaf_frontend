4a7bc448f45e7b47b345a2ca98874e7458538256
justiandevs <justiandev@gmail.com>
2021-02-07 21:42:20 +0100

    Post for create products is working. Only image API integration todo.

--- src/components/dashboard/products/createProduct.vue
+++ src/components/dashboard/products/createProduct.vue
@@ -4,6 +4,11 @@
             <div class="card-content">
                 <div class="content">
                     <h1 class="title is-4">Nieuw product toevoegen</h1>
+                    <div style="margin-bottom: 1vh;">
+                        <p class="has-text-danger">
+                            Het veld, max, staat voor de hoeveelheid producten die u beschikbaar heeft. U kiest -1 voor status uitverkocht, 0 voor status oneindig en een ander getal om het precies aan te geven.
+                        </p>
+                    </div>
                     <div v-if="back_errors.length" style="margin-bottom: 1vh;">
                         <p class="has-text-danger">Hey, er zijn wat fouten opgetreden!
                             <span v-for="back_error in back_errors" :key="back_error">{{ back_error }} </span>
@@ -18,7 +23,10 @@
                             <b-input name="productDescription" type="textarea" v-model="productDescription" placeholder="Vul hier uw productbeschrijving in"></b-input>
                         </b-field>
                         <b-field label="Prijs (optioneel)" :type="{'is-danger': true, 'is-danger': errors.has('price')}" :message="errors.first('price')">
-                            <b-input v-validate="'decimal:2'" name="price" type="text" v-model="price" placeholder="Prijs" icon="euro-sign" icon-pack="fas"></b-input>
+                            <b-input v-validate="'decimal:2|min_value:0'" name="price" type="text" v-model="price" placeholder="Prijs" icon="euro-sign" icon-pack="fas"></b-input>
+                        </b-field>
+                        <b-field label="Max (optioneel)" :type="{'is-danger': true, 'is-danger': errors.has('max')}" :message="errors.first('max')">
+                            <b-input v-validate="'decimal:0|min_value:-1'" name="max" type="text" v-model="max" placeholder="Max"></b-input>
                         </b-field>
                         <!--upload-->
                         <b-field label="Productfoto (optioneel)">
@@ -44,16 +52,36 @@ export default {
             productName: '',
             productDescription: '',
             file: '',
-            price: ''
+            price: '',
+            max: '',
+            id: localStorage.getItem('id') || '' 
         }
     },
     methods: {
         checkForm() {
-            if(this.$refs.file.files) {
-                let formData = new FormData(); 
-                formData.append('file', this.file);
-                console.log(formData);
-            }
+            // Set backend errors to null
+            this.back_errors = [];
+
+            this.$validator.validateAll().then((result) => {
+                if (result) {
+                    this.apiCall(this.productName, this.productDescription, this.price, this.max);
+                } else {
+                    this.back_errors.push('Je hebt niet alle velden correct ingevuld, probeer het opnieuw.');
+                }
+            });
+        },
+        apiCall(productName, productDescription, price, max) {
+            this.date = Date.now();
+            this.key = this.apiKey();
+
+            this.$http.post("https://dev-api.haalnuaf.nl/products/create", { key: this.key, time: this.date, storeid: this.id, name: productName, description: productDescription, price: price, max: max })
+            .then(async response => {
+                console.log(response.data);
+                this.$router.push({ name: 'overviewProduct' });
+            })
+            .catch(error => {
+                this.back_errors.push('Bericht: ' + error.response.data.msg);
+            })
         },
         handleFileUpload() {
             this.file = this.$refs.file.files[0];
