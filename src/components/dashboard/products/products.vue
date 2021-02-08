ffea3422d2909b7115a8f314b635f69597fea0aa
justiandevs <justiandev@gmail.com>
2021-02-08 10:57:27 +0100

    Error logging products done. API working.

--- src/components/dashboard/products/products.vue
+++ src/components/dashboard/products/products.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
         <div class="columns is-multiline">
-            <productCard productTitle="Voorbeeldnaam" productDescription="Voorbeeld omschrijving" price="50.00"/>
-            <productCard productTitle="Voorbeeldnaam" productDescription="Voorbeeld omschrijving. Dit is een test of de cards wel meeschalen." price="50.00"/>
-            <productCard productTitle="Voorbeeldnaam" productDescription="Voorbeeld omschrijving" price="50.00"/>
-            <productCard productTitle="Voorbeeldnaam" productDescription="Voorbeeld omschrijving" price="50.00"/>
+            <div v-if="back_errors.length" class="column">
+                <p class="has-text-danger">Hey, er zijn wat fouten opgetreden tijdens het inladen van de producten!
+                    <span v-for="back_error in back_errors" :key="back_error">{{ back_error }} </span>
+                </p>
+            </div>
             <productCard productTitle="Voorbeeldnaam" productDescription="Voorbeeld omschrijving" price="50.00"/>
             <b-button @click=apiCall()>Test</b-button>
         </div>
@@ -16,8 +17,18 @@ import productCard from './productCard.vue'
 
 export default {
     name: "producten", 
+    data() {
+        return {
+            key: '',
+            date: '',
+            back_errors: []
+        }
+    },
     components: {
         productCard
+    },
+    mounted() {
+        this.apiCall();
     },
     methods: {
         apiCall() {
@@ -25,14 +36,12 @@ export default {
             this.key = this.apiKey();
 
             let userId = localStorage.getItem('id');
-            this.$http.get(process.env.VUE_APP_API + "/users/" + userId + "/products", { key: this.key, time: this.date })
+            this.$http.get(process.env.VUE_APP_API + "/users/" + userId + "/products", { params: { key: this.key, time: this.date } })
             .then(response => {
                 console.log(response);
             })
             .catch(error => {
-                console.log(error.response.data.msg);
-                console.log(this.date);
-                console.log(this.key);
+                this.back_errors.push('Bericht: ' + error.response.data.msg)
             })
         }
     }
