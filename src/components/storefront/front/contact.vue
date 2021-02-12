d26480c8aaf4024227f46130eaef25e78c08a8b8
justiandevs <justiandev@gmail.com>
2021-02-12 14:15:04 +0100

    Fixed contact storefront.

--- src/components/storefront/front/contact.vue
+++ src/components/storefront/front/contact.vue
@@ -2,20 +2,20 @@
     <div class="content">
         <h1 class="title is-4">Contact</h1>
         <div>
-            <strong>Mailadres</strong> {{ store.contactEmail }}
+            <strong>Mailadres</strong> {{ store.contactEmail ? store.contactEmail : "niet opgegeven" }}
         </div>
         <div>
-            <strong>Telefoonnummer</strong> {{ store.contactPhone }}
+            <strong>Telefoonnummer</strong> {{ store.contactPhone ? store.contactPhone : "niet opgegeven" }}
         </div>
         <br>
         <div>
-            <strong>Adres</strong> {{ store.contactLocationStreet }}
+            <strong>Adres</strong> {{ store.contactLocationStreet ? store.contactLocationStreet : "niet opgegeven" }}
         </div>
         <div>
-            <strong>Plaatsnaam</strong> {{ store.contactLocationPlace }}
+            <strong>Plaatsnaam</strong> {{ store.contactLocationPlace ? store.contactLocationPlace : "niet opgegeven" }}
         </div>
         <div>
-            <strong>Postcode</strong> {{ store.contactLocationZip }}
+            <strong>Postcode</strong> {{ store.contactLocationZip ? store.contactLocationZip : "niet opgegeven" }}
         </div>
     </div>
 </template>
