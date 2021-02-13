2e5cadfe7e11bd912733bd0614c8dd315ae896cc
justiandevs <justiandev@gmail.com>
2021-02-13 16:01:34 +0100

    Cart is now loading correct data and doing API check if store exists.

--- src/components/storefront/cart.vue
+++ src/components/storefront/cart.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <storeNav/>
-        <hero title="Winkel" description="Alvast bedankt voor uw (mogelijke) bestelling."/>
+        <hero :title="store.company" :description="store.story" :img="store.banner"/>
         <div class="container" style="margin-top: 2vh;">
             <div class="columns">
                 <div class="column is-three-quarters">
@@ -31,6 +31,28 @@ export default {
         orderView,
         checkout,
         foot
+    },
+    data() {
+        return {
+            store: []
+        }
+    },
+    async created() {
+        this.date = Date.now(); 
+        const crypto = require('crypto');
+        this.key = encodeURIComponent(crypto.createHash('sha256').update(this.date + "---" + process.env.VUE_APP_SALT).digest('base64'));
+
+        await this.$http.get(process.env.VUE_APP_API + '/stores/' + this.$route.params.id, { params: { key: this.key, time: this.date }})
+        .then(async response => {
+            this.store = await response.data.store; 
+        })
+        .catch(error => {
+            if(error.response.data.status === 404) {
+                this.$router.push('/404');
+            } else {
+                this.$router.push('/404');
+            }
+        })
     }
 }
 </script>
